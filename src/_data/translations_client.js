724f1b0a718842b54f25af366c325569b95fce9a
Thomas Francart <thomas.francart@sparna.fr>
2025-08-20 00:14:24 +0200

    refactor of label management

--- src/_data/translations_client.js
+++ src/_data/translations_client.js
@@ -9,33 +9,43 @@ const file_rico_en = "./src/_data/i18n/en/rico.csv";
 const rico_fr = fs.readFileSync(file_rico_fr, { encoding: "utf-8", flag: "r" });
 const rico_en = fs.readFileSync(file_rico_en, { encoding: "utf-8", flag: "r" });
 
+const file_anfonto_fr = "./src/_data/i18n/fr/anf-onto.csv";
+const file_anfonto_en = "./src/_data/i18n/en/anf-onto.csv";
+const anfonto_fr = fs.readFileSync(file_anfonto_fr, { encoding: "utf-8", flag: "r" });
+const anfonto_en = fs.readFileSync(file_anfonto_en, { encoding: "utf-8", flag: "r" });
+
+const file_otheronto_fr = "./src/_data/i18n/fr/other-ontologies.csv";
+const file_otheronto_en = "./src/_data/i18n/en/other-ontologies.csv";
+const otheronto_fr = fs.readFileSync(file_otheronto_fr, { encoding: "utf-8", flag: "r" });
+const otheronto_en = fs.readFileSync(file_otheronto_en, { encoding: "utf-8", flag: "r" });
+
 const file_rico_fr_definition = "./src/_data/i18n/fr/definition.csv";
 const file_rico_en_definition = "./src/_data/i18n/en/definition.csv";
 const rico_fr_definition = fs.readFileSync(file_rico_fr_definition, { encoding: "utf-8", flag: "r" });
 const rico_en_definition = fs.readFileSync(file_rico_en_definition, { encoding: "utf-8", flag: "r" });
 
-// fonction pour lire les données d'un fichier csv et retourner une estructure de dictionaire
-const dicFR = readCSV(rico_fr);
-const dicEN = readCSV(rico_en);
-const dicFR_definition = readCSV_for_Tooltip(rico_fr_definition);
-const dicEN_definition = readCSV_for_Tooltip(rico_en_definition);
-
 const translations = {
   en: Object.assign(
     {},
-    Object.assign({}, dicEN, dicEN_definition),
+    readCSV(rico_en),
+    readCSV(anfonto_en),
+    readCSV(otheronto_en),
+    readCSV_for_Tooltip(rico_en_definition),
     static_translations_en
   ),
   fr: Object.assign(
     {},
-    Object.assign({}, dicFR, dicFR_definition),
+    readCSV(rico_fr),
+    readCSV(anfonto_fr),
+    readCSV(otheronto_fr),
+    readCSV_for_Tooltip(rico_fr_definition),
     static_translations_fr
   ),
 };
 
 module.exports = translations;
 
-
+// fonction pour lire les données d'un fichier csv et retourner une estructure de dictionaire
 function readCSV(data) {
   // creer l'objetc
   //const objLanguage = new Object();
@@ -46,10 +56,7 @@ function readCSV(data) {
   data_clean.forEach((l) => {
     const key = l.split(",")[0];
     const value = l.split(",")[1];
-
-    if (key !== "ricoId" && key !== "") {
-        entries.set(key, value);
-    }
+    entries.set(key,value);
   });
 
   return Object.fromEntries(entries);
@@ -63,10 +70,7 @@ function readCSV_for_Tooltip(data) {
   data_clean.forEach((l) => {
     const key = l.split(",")[0] + "_tooltip";
     const value = l.split(",")[1];
-
-    if (key !== "ricoId" && key !== "") {
-      entries.set(key, value);
-    }
+    entries.set(key,value);
   });
   return Object.fromEntries(entries);
 }
