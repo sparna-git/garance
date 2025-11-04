2332033428f306a70cada57df7d9dd3aab3db9f1
AQUINO SANCHEZ Jorge <jorge.aquinosanchez@ext.europarl.europa.eu>
2025-11-04 16:15:12 +0100

    Add #90 Implémenter la négociation de contenu sur les fichiers

---
+++ scripts/negotiation.js
@@ -0,0 +1,171 @@
+const fs = require("fs");
+//npm i glob
+const { glob, globSync } = require("glob");
+// npm install path
+const path = require("path");
+
+function getIdentifiers(jsonResource, eNode) {
+  
+  const listOfURL = []
+
+  const jsonData = JSON.parse(jsonResource);
+  const jsonArray = jsonData.graph;
+  for (let obj of jsonArray) {
+    if (Object.keys(obj).includes(eNode)) {      
+      const dataDigital = obj[eNode]["rico:hasOrHadDigitalInstantiation"];
+      if (dataDigital) {
+        if (Array.isArray(dataDigital)) {
+          for (let urlResource of dataDigital) {
+            const isRDF = urlResource["dc:format"] === "application/rdf+xml";
+            if (isRDF) {
+              const url = String(urlResource["dcat:downloadURL"].id);
+              const idAgent = url.split("/")
+              listOfURL.push(idAgent[idAgent.length - 1]);
+            }
+          }
+        }
+      }
+    }
+  }
+  return listOfURL;
+}
+
+function getAllListFilesResources(directoryResource) {
+  //
+  const allResources = globSync("**/*.rdf", { cwd: directoryResource }); 
+  //
+  const objResource = []
+  allResources.forEach((element) => {
+    const e = element.split("\\");
+    objResource.push({ name: e[e.length - 1], path: element });
+});
+  return objResource;
+}
+
+function copyResourceAgents(dirDist,pathResource) {
+  // 
+  const distWeb = fs.readdirSync(dirDist);
+  for (let agent of pathResource) {
+    const name = agent["name"];
+    let name_of_dist = ""
+    if (name.split("_").length === 3) {
+      name_of_dist = name.replace("FRAN_Agent_","").replace(".rdf","");
+    } else {
+      name_of_dist = name.replace("_Agent_", "_").replace(".rdf", "");
+    }
+
+    const pathResource = agent["path"];
+    console.log(name_of_dist);
+    if (distWeb.includes(name_of_dist)) {
+      console.log("Agent Id: " + name_of_dist + " Path: " + path);
+      // path target
+      const dirTarget = dirDist + "/" + name_of_dist + "/";
+      console.log(dirTarget);
+      // Copy File
+      fs.copyFile(pathResource, dirTarget+'data.rdf', (err) => {
+        if (err) throw err;
+        console.log("The file was copied...");
+      });
+    }
+  }
+}
+
+function copyResource(dirDist, pathResource) {
+  //
+  const distWeb = fs.readdirSync(dirDist);
+  for (let agent of pathResource) {
+    const name = agent["name"];
+    let name_of_dist = name.replace("_Place_", "_").replace(".rdf", "");    
+    const pathResource = agent["path"];
+
+    if (distWeb.includes(name_of_dist)) {
+      console.log("Agent Id: " + name_of_dist + " Path: " + pathResource);
+      // path target
+      const dirTarget = dirDist + "/" + name_of_dist + "/";
+      // Copy File
+      fs.copyFile(pathResource, dirTarget + "data.rdf", (err) => {
+        if (err) throw err;
+        console.log("The file was copied...");
+      });
+    }
+  }
+}
+
+function resourcesAgents(
+  listOfIdAgents,
+  directoryAgentReferentiels,
+  distAgents
+) {
+  // ist of files Referentiels Agents
+  console.log("Load all file in Referentiel Agents")
+  const allResourcesRDFAgents = getAllListFilesResources(directoryAgentReferentiels);
+  // Read Frame JSON Agents Header and get the identifier donwload RDF
+  console.log("Validate if an Agents Id exist in the referentiel Agents");
+  const pathAgentsFiles = [];
+  listOfIdAgents.forEach((agentId) => {
+    const getResult = allResourcesRDFAgents.filter(function (data) {
+      return data.name === agentId;
+    });
+    if (getResult) {
+      // build the full path of the file
+      const filePath = path.join(directoryAgentReferentiels, getResult[0].path);
+      pathAgentsFiles.push({ name: agentId, path: filePath });
+    } else {
+      pathAgentsFiles.push({ name: agentId, path: "" });
+    }
+  });
+  // Copy all files Agents in the dist directory
+  copyResourceAgents(distAgents, pathAgentsFiles);
+}
+
+function resourcesPlaces(
+  listOfIdPlaces,
+  directoryPlacesReferentiels,
+  distPlaces
+) {
+  // ist of files Referentiels Agents
+  console.log("Load all file in Referentiel Agents");
+  const allResourcesRDFPlaces = getAllListFilesResources(
+    directoryPlacesReferentiels
+  );
+  console.log("Validate if an Agents Id exist in the referentiel Agents");
+  const pathPlacesFiles = [];
+  listOfIdPlaces.forEach((placeId) => {
+    const getResult = allResourcesRDFPlaces.filter(function (data) {
+      return data.name === placeId;
+    });
+    if (getResult.length > 0) {
+      // build the full path of the file
+      const filePath = path.join(
+        directoryPlacesReferentiels,
+        getResult[0].path
+      );
+      pathPlacesFiles.push({ name: placeId, path: filePath });
+    } else {
+      pathPlacesFiles.push({ name: placeId, path: "" });
+    }
+  });
+  // Copy all files Agents in the dist directory
+  copyResource(distPlaces, pathPlacesFiles);
+}
+
+(async () => {
+  
+  // --- Agents  ---
+  const directoryAgentReferentiels = "./Referentiels/agents";
+  const distAgents = "./dist/entities/agent";
+  console.log("Reading " + "src/_data/agentsHeader.json" + " for Agents...");
+  let dataJsonAgents = fs.readFileSync("src/_data/agentsHeader.json");
+  // Get list of file name
+  let listOfIdAgents = getIdentifiers(dataJsonAgents,"rico:isOrWasDescribedBy");
+  resourcesAgents(listOfIdAgents, directoryAgentReferentiels, distAgents);
+  
+  // Places
+  const directoryPlaceReferentiels = "./Referentiels/lieux";
+  const distPlace = "./dist/entities/place";
+  console.log("Reading " + "src/_data/placesHeader.json" + " for places...");
+  let dataJsonPlaces = fs.readFileSync("src/_data/placesHeader.json");
+  // Get list of file name
+  let listOfIdPlaces = getIdentifiers(dataJsonPlaces,"rico:isOrWasDescribedBy");
+  resourcesPlaces(listOfIdPlaces, directoryPlaceReferentiels, distPlace);
+})();
